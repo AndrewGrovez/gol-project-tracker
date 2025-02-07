915affc7162106ef246c10894079c034493fd808
AndrewGrovez <andrew@golcentres.co.uk>
2025-02-07 13:01:22 -0500

    fixed param issue

--- src/app/projects/[id]/page.tsx
+++ src/app/projects/[id]/page.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ProjectDetails from '@/components/ProjectDetails'
 
-export default async function ProjectPage({ params }: { params: { id: string } }) {
-  return <ProjectDetails id={params.id} />
+export default async function ProjectPage({ params }: { params: Promise<{ id: string }> }) {
+  const { id } = await params
+  return <ProjectDetails id={id} />
 }
